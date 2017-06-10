5c5771ba06110d174136716ec587bf3e87f7c986
Ali <ozdemirali@hotmaiil.com>
2017-06-11 00:43:13 +0300

    Ekran görüntüleri

---
+++ TransmitterWEB/Scripts/App/Home/index.js
@@ -0,0 +1,45 @@
+﻿var app = angular.module("myApp", []);
+app.controller("myCtrl", function ($scope) {
+    $scope.deneme = "asdas";
+    $scope.tabVal = 1;
+
+    $scope.nextTab = function () {
+
+        if ($scope.tabVal < 3) {
+            $scope.tabVal++;
+        }
+
+    }
+
+
+    $scope.rows=[{
+        id: 1,
+        fieldName: "",
+        fieldType: "",
+
+    }
+       
+    ];
+   
+    $scope.counter = 2;
+
+    $scope.addRow = function () {
+
+       
+       
+
+        $scope.rows.push({
+            id: $scope.counter,
+            fieldName: "",
+            fieldType: ""
+        });
+        $scope.counter++;
+    }
+
+    /**********************************************************************/
+    $scope.$watch('rows', function (model) {
+        $scope.modelAsJson = angular.toJson(model, true)
+        console.log("asd");
+    }, true);
+
+});
