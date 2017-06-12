38a38ef26e2993c6dfdb7135e67a39b683db8e9a
Ali <ozdemirali@hotmaiil.com>
2017-06-12 17:23:11 +0300

    angular-material eklendi

--- TransmitterWEB/Scripts_/app.js
+++ TransmitterWEB/Scripts_/app.js
@@ -1,4 +1,4 @@
-﻿var app = angular.module("myApp", ['nvd3']);
+﻿var app = angular.module("myApp", ['nvd3', 'ngMaterial']);
 app.controller("navigationCtrl", ['$scope', '$http', function ($scope, $http) {
     $scope.tabVal = 1;
     $scope.counter = 2;
